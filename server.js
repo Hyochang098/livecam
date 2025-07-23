a80516db9a036bfce7e434dcb1d88032ff4d95a0
Hyochang098 <wlsgyckd7@naver.com>
2025-07-24 02:33:13 +0900

    feat: 라이브 송출 구현

--- server.js
+++ server.js
@@ -19,12 +19,17 @@ const wss = new WebSocketServer({ server });
 const rooms = new Map();          // roomId → Set(ws)
 
 wss.on('connection', (ws, req) => {
-  const roomId = new URL(req.url, 'http://x').pathname.split('/').pop(); // /ws/room1
+  const roomId = new URL(req.url, 'http://x').pathname.split('/').pop();
   if (!rooms.has(roomId)) rooms.set(roomId, new Set());
   rooms.get(roomId).add(ws);
 
-  ws.on('message', d => rooms.get(roomId).forEach(p => p !== ws && p.send(d)));
-  ws.on('close',  () => rooms.get(roomId).delete(ws));
+  ws.on('message', data => {
+   const text = typeof data === 'string' ? data : data.toString();
+   rooms.get(roomId).forEach(peer => peer !== ws && peer.send(text));
+  });
+
+  ws.on('close', () => rooms.get(roomId).delete(ws));
 });
+
 
 server.listen(3000, () => console.log('http/ws :3000'));
